c5c9613bdafe29222ddf86c9c493039074ed80e2
Mark <mgaza@texasfile.com>
2022-01-17 10:38:58 -0600

    Added comments and new error check

--- goTools.go
+++ goTools.go
@@ -1,3 +1,11 @@
+/*
+	These functions are for generally coding practices using the
+	Go Language. All of these are used frequently in our data engineering
+	day to day.
+
+	Author: Mark
+*/
+
 package goTools
 
 import (
@@ -8,12 +16,24 @@ import (
 	"regexp"
 )
 
+// Use to check for errors that should
+// not logically allow the program to continue
 func CheckErrorFatal(message string, err error) {
 	if err != nil {
 		log.Fatal(message, err)
 	}
 }
 
+// Use for testing errors that could allow the
+// program to pass or during testing stages
+func CheckErrorNonFatal(message string, err error) {
+	if err != nil {
+		fmt.Println(message, err)
+	}
+}
+
+// Allows for a slice return of file searches
+// down whatever path the user wants to go down
 func FilePathWalker(filePath string, extfile string) []string {
 	re := regexp.MustCompile(extfile)
 	var paths []string
